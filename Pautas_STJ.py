e7d47395a6644987e7bab937139ef562511f7248
MarceloDC2 <marcelodc2@gmail.com>
2025-11-13 13:47:35 -0300

    Add files via upload

--- Pautas_STJ.py
+++ Pautas_STJ.py
@@ -54,8 +54,13 @@ if st.button("🔍 Buscar e gerar ZIP"):
         total_datas = len(datas)
 
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36'
-        }
+                      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                                    "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                      "Referer": "https://processo.stj.jus.br/",
+                      "Accept": "application/pdf,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                      "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+                      "Connection": "keep-alive",
+                  }
 
         for i, data in enumerate(datas, start=1):
             progresso.progress(i / total_datas)
